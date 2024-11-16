bab4ac3866ab200893bd3e6361689e0c662cff86
rummansaaqeb <rsaaqeb@gmail.com>
2024-11-16 13:24:44 +0600

    added createContext and useContext

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
 
 const Navbar = () => {
+
+    const { name } = useContext(AuthContext);
+
+    console.log(name);
 
     const links = <>
         <li><NavLink to='/'>Home</NavLink></li>
@@ -40,7 +45,7 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <a className="btn">{name}</a>
             </div>
         </div>
     );
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { createContext } from 'react';
 
-const AuthProvider = () => {
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+
+    const authInfo = {
+        name: "Nodier Shagor"
+    }
+
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
 export default AuthProvider;
+
+
+/**
+ * 1. Create context with null as default value
+ * 2. Create Provider
+ * 3. Set a value with something (authInfo)
+ * 4. [attention please !!!!!]
+ * 5. use the auth provider in the main.jsx
+ * 6. access the children inside the auth provider in the main.jsx 
+ * 7. export authContext
+ */
